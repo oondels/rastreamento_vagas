c93ad3c0036ae5c9e49d2818bf1558b10d53169f
Hendrius Felix <hendriusfelix@gmail.com>
2026-04-27 10:33:41 -0300

    Add basic TTP223 sensor mapping

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+constexpr uint8_t sensorPins[] = {33, 25, 26, 27};
+constexpr size_t sensorCount = sizeof(sensorPins) / sizeof(sensorPins[0]);
+
+bool lastSensorStates[sensorCount] = {false, false, false, false};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  for (size_t i = 0; i < sensorCount; i++) {
+    pinMode(sensorPins[i], INPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  for (size_t i = 0; i < sensorCount; i++) {
+    bool currentState = digitalRead(sensorPins[i]) == HIGH;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (currentState && !lastSensorStates[i]) {
+      Serial.print("Sensor ");
+      Serial.print(i + 1);
+      Serial.println(" detectado");
+    }
+
+    lastSensorStates[i] = currentState;
+  }
+
+  delay(50);
 }
